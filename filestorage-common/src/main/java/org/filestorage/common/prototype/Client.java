b41356aa1bcb5968277c2ac62216a0dd83e29fb2
andrey_kazakov <aakazakovv@yandex.ru>
2020-07-22 17:00:38 +0300

    rewrite code

--- filestorage-common/src/main/java/org/filestorage/common/prototype/Client.java
+++ filestorage-common/src/main/java/org/filestorage/common/prototype/Client.java
@@ -4,56 +4,25 @@ import java.io.*;
 import java.net.Socket;
 
 public class Client {
-  Socket socket;
-  
-  public Client() {
-    socket = null;
-    connect();
-  }
-
   public static void main(String[] args) {
     
-    Client client = new Client();
-    client.showListOfFiles();
-    
   }
   
-  public void connect() {
-    try {
-      socket = new Socket(Common.HOST, Common.PORT);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
-  
-  public void disConnect() {
-    if (socket == null || socket.isClosed()) { return; }
-    try {
-      socket.close();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
-  
-  public void put(File file) {
-    try (InputStream is = new FileInputStream(file);
-         DataOutputStream os = new DataOutputStream(socket.getOutputStream());) {
+  private void put(File file) {
+    try (Socket socket = new Socket(Common.HOST, Common.PORT);
+        InputStream in = new FileInputStream(file);
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());) {
+      
       byte[] buffer = new byte[8192];
-      int read;
-      os.writeUTF(file.getName());
-      while ((read = is.read(buffer)) != -1) {
-        os.write(buffer, 0, read);
-      }
+      int edge;
+      
+      out.writeUTF(file.getName());
+      
+      while ((edge = in.read()) != -1) {
+        out.write(buffer, 0, edge);
+      }         
     } catch (IOException e) {
       e.printStackTrace();
     }
-  }
-  
-  public void get(String fileName) {
-    
-  }
-  
-  public void showListOfFiles() {
-    System.out.println("list");
   }
 }
