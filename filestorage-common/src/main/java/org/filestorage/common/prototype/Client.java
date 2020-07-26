b0998c56f8d0f65cfdb8c6c23cea7b18dcfd0eb5
andrey_kazakov <aakazakovv@yandex.ru>
2020-07-26 12:36:36 +0300

    server can put files

--- filestorage-common/src/main/java/org/filestorage/common/prototype/Client.java
+++ filestorage-common/src/main/java/org/filestorage/common/prototype/Client.java
@@ -6,83 +6,74 @@ import java.util.Scanner;
 
 public class Client {
   Socket socket;
+  DataInputStream in;
+  DataOutputStream out;
   
-  public Client() {
+  public Client() throws IOException {
+    connect();
     interaction();
   }
   
   public static void main(String[] args) {
-    new Client();
+    try {
+      new Client();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   } 
   
   private void connect() throws IOException {
     socket = new Socket(Common.HOST, Common.PORT);
+    out = new DataOutputStream(socket.getOutputStream());
+    in = new DataInputStream(socket.getInputStream());
   }
   
-  private void interaction() {
+  private void interaction() throws IOException {
     Scanner scanner = new Scanner(System.in);
-    StringBuilder query = new StringBuilder();
     while (true) {
-      query.setLength(0);
-      
       System.out.print("Enter the command: ");
       String command = scanner.nextLine();
+      
       if (command.equals(Common.EXIT_CODE)) break;
-      query.append(command).append(Common.DELIMETER);
       
-      System.out.print("Enter filename (if needed or press `Enter`): ");
-      query.append(scanner.nextLine()).append(Common.DELIMETER);
-      
-      System.out.print("Enter path (if needed or press `Enter`): ");
-      query.append(scanner.nextLine());
-      
-      send(query.toString());
+      String file;
       
       if (command.equals(Common.GET_FILE_CODE)) {
         System.out.print("Enter filename: ");
-      }     
-           
-      if (command.equals(Common.PUT_CODE)) {
-        put(scanner.nextLine());
+        file = scanner.nextLine();
+        get(file);
+      }
+      
+      if (command.equals(Common.PUT_FILE_CODE)) {
+        System.out.print("Enter path: ");
+        file = scanner.nextLine();
+        put(file);
+      }
+      
+      if (command.equals(Common.GET_LIST_CODE)) {
+        System.out.println("files list");
       }
     }   
     scanner.close();
   }
   
-  private void send(String query) { // Parsing is here )) ?????
-    try (DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
-      out.writeUTF(query);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
-  
-  private void put(String src) {
-    try (DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
-      File file = new File(src);
-      FileInputStream fis = new FileInputStream(file);
+  private void put(String src) throws IOException {
+    File file = new File(src);
+    out.writeUTF(Common.PUT_FILE_CODE);
+    out.writeUTF(file.getName());
+    
+    try (FileInputStream fis = new FileInputStream(file)) {
       byte[] buffer = new byte[8192];
       int edge;
       while (fis.available() > 0) {
         edge = fis.read(buffer);
         out.write(buffer, 0, edge);
       }
-      out.flush();
-      fis.close();
-    } catch (IOException e) {
-      e.printStackTrace();
     }
   }
   
   private void get(String fileName) {
-    try (DataInputStream in = new DataInputStream(socket.getInputStream());
-        DataOutputStream out = new DataOutputStream(socket.getOutputStream());) {
-      
-      
-      
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+
   }
   
   private void getList() {
--- filestorage-common/src/main/java/org/filestorage/common/prototype/Server.java
+++ filestorage-common/src/main/java/org/filestorage/common/prototype/Server.java
@@ -1,1 +1,101 @@
 package org.filestorage.common.prototype;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.ServerSocket;
+
+public class Server {
+  private class ClientRequestsHandler implements Runnable {
+    Socket socket;
+    DataInputStream in;
+    DataOutputStream out;
+    
+    public ClientRequestsHandler(Socket socket) throws IOException {
+      this.socket = socket;
+      in = new DataInputStream(socket.getInputStream());
+      out = new DataOutputStream(socket.getOutputStream());
+    }
+
+    @Override
+    public void run() {
+      System.out.println("Client in thread...");
+      
+      while (isRunning || !socket.isClosed()) {
+        try {         
+          String request = in.readUTF();
+          
+          if (request.equals(Common.PUT_FILE_CODE)) {
+            request = in.readUTF();
+            put(request);
+          }
+          
+          if (request.equals(Common.GET_FILE_CODE)) {
+            get();
+          }
+          
+          if (request.equals(Common.GET_LIST_CODE)) {
+            getList();
+          }
+          
+        } catch (IOException e) {
+          throw new RuntimeException(e.getMessage()); // TODO need to handle exception when the socket falls.
+        }
+      }
+    }
+    
+    private void put(String filename) throws IOException {
+      File file = new File(Common.PATH_TO_SERVER_STORAGE + filename);
+      if (file.exists()) file.delete();
+      file.createNewFile();
+      
+      try (FileOutputStream fos = new FileOutputStream(file)) {
+        byte[] buffer = new byte[1024];
+        int edge;        
+        while (in.available() > 0) {
+          edge = in.read(buffer);
+          fos.write(buffer, 0, edge);
+        }
+        System.out.println("File in server storage, file length: " + file.length());
+      }  
+    }
+    
+    private void get() {
+      System.out.println("Server get file");
+    }
+    
+    private void getList() {
+      System.out.println("Server list file");
+    }
+  }
+  
+  private ServerSocket server;
+  private boolean isRunning = false;
+  
+  public Server() throws IOException {
+    start();
+  }
+  
+  public static void main(String[] args) {
+    try {
+      new Server();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+  
+  public void start() throws IOException {
+    isRunning = true;
+    server = new ServerSocket(Common.PORT);
+    System.out.println("Server started...");
+    
+    while (isRunning) {
+      Socket socket = server.accept();
+      System.out.println("Client connected...");
+      new Thread(new ClientRequestsHandler(socket)).start();
+    }
+  }
+  
+  public void stop() {
+    isRunning = false;
+  }
+}
