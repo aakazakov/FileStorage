bf154ad7d298f60032eae77d4c98d17b060d0876
andrey_kazakov <aakazakovv@yandex.ru>
2020-07-26 12:35:01 +0300

    fix paths

--- filestorage-common/src/main/java/org/filestorage/common/prototype/Common.java
+++ filestorage-common/src/main/java/org/filestorage/common/prototype/Common.java
@@ -7,12 +7,12 @@ public class Common {
   
   public static final String DELIMETER = "☢";
   public static final String EXIT_CODE = "/q";
-  public static final String PUT_CODE = "/put";
+  public static final String PUT_FILE_CODE = "/put";
   public static final String GET_FILE_CODE = "/getfile";
   public static final String GET_LIST_CODE = "/getlist";
   
-  public static final String PATH_TO_SERVER_STORAGE = "./src/main/java/org/filestorage/common/prototype/server_storage/";
-  public static final String PATH_TO_CLIENT_STORAGE = "./src/main/java/org/filestorage/common/prototype/client_storage/";
+  public static final String PATH_TO_SERVER_STORAGE = "src/main/java/org/filestorage/common/prototype/server_storage/";
+  public static final String PATH_TO_CLIENT_STORAGE = "src/main/java/org/filestorage/common/prototype/client_storage/";
   
   private Common() {
     
