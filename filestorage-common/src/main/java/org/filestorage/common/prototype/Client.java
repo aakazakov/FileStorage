06c491d1dd04d6534218d1982dcebde4309675ed
andrey_kazakov <aakazakovv@yandex.ru>
2020-07-29 11:55:02 +0300

    one client can transfer to the server storage one file

--- filestorage-common/src/main/java/org/filestorage/common/prototype/Client.java
+++ filestorage-common/src/main/java/org/filestorage/common/prototype/Client.java
@@ -6,103 +6,51 @@ import java.util.Scanner;
 
 public class Client {
   Socket socket;
-  DataInputStream in;
-  DataOutputStream out;
-  
+
   public Client() throws IOException {
     connect();
-    interaction();
+    put(new File("d:/tmp/test.pdf"));
+  }
+
+  private void connect() throws IOException {
+    socket = new Socket(Common.HOST, Common.PORT);
   }
   
+  @SuppressWarnings("unused")
+  private void disconnect() throws IOException {
+    socket.close();
+  }
+
+  private void put(File file) throws IOException {
+    if (file.exists()) {
+
+      try (DataInputStream in = new DataInputStream(socket.getInputStream());
+          DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+          InputStream fis = new FileInputStream(file);) {
+        System.out.println("i/o streams has been created...");
+
+        out.writeUTF(file.getName());
+        byte[] buffer = new byte[8192];
+        int edge;
+
+        System.out.println("Bytes are ready to be transmitted...");
+        while (fis.available() > 0) {
+          edge = fis.read(buffer);
+          out.write(buffer, 0, edge);
+        }
+      }
+
+      System.out.println("==Client== file length: " + file.length());
+    } else {
+      System.out.println("File does not exist...");
+    }
+  }
+
   public static void main(String[] args) {
     try {
       new Client();
     } catch (IOException e) {
       e.printStackTrace();
     }
-  } 
-  
-  private void connect() throws IOException {
-    socket = new Socket(Common.HOST, Common.PORT);
-    out = new DataOutputStream(socket.getOutputStream());
-    in = new DataInputStream(socket.getInputStream());
-  }
-  
-  private void interaction() throws IOException {
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      System.out.print("Enter the command: ");
-      String command = scanner.nextLine();
-      
-      if (command.equals(Common.EXIT_CODE)) break;
-      
-      String file;
-      
-      if (command.equals(Common.PUT_FILE_CODE)) {
-        System.out.print("Enter path: ");
-        file = scanner.nextLine();
-        put(file);
-      }
-      
-      if (command.equals(Common.GET_FILE_CODE)) {
-        System.out.print("Enter filename: ");
-        file = scanner.nextLine();
-        get(file);
-      }
-      
-      if (command.equals(Common.GET_LIST_CODE)) {
-        getList();
-      }
-    }   
-    scanner.close();
-  }
-  
-  private void put(String src) throws IOException {
-    File file = new File(src);
-    if (file.exists()) {
-      out.writeUTF(Common.PUT_FILE_CODE);
-      out.writeUTF(file.getName());
-      
-      try (FileInputStream fis = new FileInputStream(file)) {
-        byte[] buffer = new byte[8192];
-        int edge;
-        while (fis.available() > 0) {
-          edge = fis.read(buffer);
-          out.write(buffer, 0, edge);
-        }
-      }
-    } else {
-      System.out.println("file not found by path: " + src);
-    } 
-  }
-  
-  private void get(String filename) throws IOException {
-    out.writeUTF(Common.GET_FILE_CODE);
-    out.writeUTF(filename);
-    String response = in.readUTF();
-    if (response.equals(Common.OK_STATUS)) {
-      File file = new File(Common.PATH_TO_CLIENT_STORAGE + filename);
-      if (file.exists()) file.delete();
-      file.createNewFile();
-      
-      try (FileOutputStream fos = new FileOutputStream(file)) {
-        byte[] buffer = new byte[8192];
-        int edge;
-        while (in.available() > 0) {
-          edge = in.read(buffer);
-          fos.write(buffer, 0, edge);
-        }
-        System.out.println("File in the client storage, file length: " + file.length());
-      }
-      
-    } else {
-      System.out.println("There is no such file in the server storage: " + filename);
-    }
-  }
-  
-  private void getList() throws IOException {
-    out.writeUTF(Common.GET_LIST_CODE);
-    String list = in.readUTF();
-    System.out.println(list);
   }
 }
--- filestorage-common/src/main/java/org/filestorage/common/prototype/Server.java
+++ filestorage-common/src/main/java/org/filestorage/common/prototype/Server.java
@@ -5,99 +5,48 @@ import java.net.Socket;
 import java.net.ServerSocket;
 
 public class Server {
-  private class ClientRequestsHandler implements Runnable {
-    Socket socket;
-    DataInputStream in;
-    DataOutputStream out;
-    
-    public ClientRequestsHandler(Socket socket) throws IOException {
-      this.socket = socket;
-      in = new DataInputStream(socket.getInputStream());
-      out = new DataOutputStream(socket.getOutputStream());
-    }
-
-    @Override
-    public void run() {
-      System.out.println("Client in thread...");
-      
-      while (isRunning) {
-        try {         
-          String request = in.readUTF();
-          
-          if (request.equals(Common.PUT_FILE_CODE)) {
-            request = in.readUTF();
-            put(request);
-          }
-          
-          if (request.equals(Common.GET_FILE_CODE)) {
-            request = in.readUTF();
-            get(request);
-          }
-          
-          if (request.equals(Common.GET_LIST_CODE)) {
-            getList();
-          }
-          
-        } catch (IOException e) {
-          throw new RuntimeException(e.getMessage()); // TODO need to handle exception when the socket falls.
-        }
-      }
-    }
-    
-    private void put(String filename) throws IOException {
-      File file = new File(Common.PATH_TO_SERVER_STORAGE + filename);
-      if (file.exists()) file.delete();
-      file.createNewFile();
-      
-      try (FileOutputStream fos = new FileOutputStream(file)) {
-        byte[] buffer = new byte[8192];
-        int edge;        
-        while (in.available() > 0) {
-          edge = in.read(buffer);
-          fos.write(buffer, 0, edge);
-        }
-        System.out.println("File in the server storage, file length: " + file.length());
-      }  
-    }
-    
-    private void get(String filename) throws IOException {
-      File file = new File(Common.PATH_TO_SERVER_STORAGE + filename);
-      if (file.exists()) {
-        out.writeUTF(Common.OK_STATUS);
-        
-        try (FileInputStream fis = new FileInputStream(file)) {
-          byte[] buffer = new byte[8192];
-          int edge;
-          while (fis.available() > 0) {
-            edge = fis.read(buffer);
-            out.write(buffer, 0, edge);
-          }
-        }
-        
-      } else {
-        out.writeUTF(Common.FAIL_STATUS);
-      }
-    }
-    
-    private void getList() throws IOException {
-      File file = new File(Common.PATH_TO_SERVER_STORAGE);
-      String[] list = file.list();
-      StringBuilder formatString = new StringBuilder();
-      for (int i = 0; i < list.length; i++) {
-        formatString.append(list[i]);
-        if (i != list.length - 1) {
-          formatString.append("\n");
-        }
-      }
-      out.writeUTF(formatString.toString());
-    }
-  }
-  
-  private ServerSocket server;
-  private boolean isRunning = false;
+  ServerSocket server;
+  DataInputStream in;
+  DataOutputStream out;
   
   public Server() throws IOException {
     start();
+  }
+  
+  private void start() throws IOException {
+    server = new ServerSocket(Common.PORT);
+    System.out.println("Server has been started...");
+    
+    Socket socket = server.accept();
+    System.out.println("Client has been accepted...");
+    
+    in = new DataInputStream(socket.getInputStream());
+    out = new DataOutputStream(socket.getOutputStream());
+    System.out.println("i/o streams has been created...");
+    
+    String filename = in.readUTF();
+    
+    File file = new File(Common.PATH_TO_SERVER_STORAGE + filename);
+    if (file.exists()) file.delete();
+    file.createNewFile();
+    
+    try (OutputStream fos = new FileOutputStream(file)) {
+      byte[] buffer = new byte[8192];
+      int edge;
+      
+      System.out.println("Bytes are ready to be recieved...");
+      while ((edge = in.read(buffer)) != -1) {
+        fos.write(buffer, 0, edge);
+      }
+      
+      System.out.println("==Server== file length: " + file.length());
+    }
+  }
+  
+  @SuppressWarnings("unused")
+  private void stop() throws IOException {
+    server.close();
+    System.out.println("Server has been stopped...");
   }
   
   public static void main(String[] args) {
@@ -106,21 +55,5 @@ public class Server {
     } catch (IOException e) {
       e.printStackTrace();
     }
-  }
-  
-  public void start() throws IOException {
-    isRunning = true;
-    server = new ServerSocket(Common.PORT);
-    System.out.println("Server started...");
-    
-    while (isRunning) {
-      Socket socket = server.accept();
-      System.out.println("Client connected...");
-      new Thread(new ClientRequestsHandler(socket)).start();
-    }
-  }
-  
-  public void stop() {
-    isRunning = false;
   }
 }
