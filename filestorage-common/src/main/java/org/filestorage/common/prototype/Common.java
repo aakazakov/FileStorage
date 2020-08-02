05c77d55a3efec7249394296f97c70d2946928d9
andrey_kazakov <aakazakovv@yandex.ru>
2020-08-02 22:29:18 +0300

    add encoding metods

--- filestorage-common/src/main/java/org/filestorage/common/prototype/Common.java
+++ filestorage-common/src/main/java/org/filestorage/common/prototype/Common.java
@@ -1,11 +1,10 @@
 package org.filestorage.common.prototype;
 
 /** different stuff) */
-public class Common {
+public class Common { 
   public static final String HOST = "127.0.0.1";
   public static final int PORT = 8189;
   
-  public static final String DELIMETER = "☢";
   public static final String EXIT_CODE = "/q";
   public static final String PUT_FILE_CODE = "/put";
   public static final String GET_FILE_CODE = "/getfile";
@@ -18,5 +17,23 @@ public class Common {
   
   private Common() {
     
+  }
+  
+  public static byte[] longToBytes(long num) {
+    byte[] bytes = new byte[Long.BYTES];
+    for (int i = 7; i >= 0; i--) {
+      bytes[i] = (byte) (num & 0xff);
+      num >>= 8;
+    }
+    return bytes;
+  }
+  
+  public static long bytesToLong(byte[] bytes) {
+    long num = 0;
+    for (int i = 0; i < 8; i++) {
+      num <<= 8;
+      num |= (bytes[i] & 0xFF);
+    }
+    return num;
   }
 }
