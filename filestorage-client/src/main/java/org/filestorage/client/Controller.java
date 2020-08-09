bc08b0576760463c771ad2ea5041c09ec0f544bf
andrey_kazakov <aakazakovv@yandex.ru>
2020-08-09 16:22:43 +0300

    preparation)

--- filestorage-client/src/main/java/org/filestorage/client/Controller.java
+++ filestorage-client/src/main/java/org/filestorage/client/Controller.java
@@ -1,5 +1,23 @@
 package org.filestorage.client;
 
-public class Controller {
-  
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+
+public class Controller implements Initializable {
+  @FXML
+  protected TableView<?> fileTable;
+
+  public void upload(ActionEvent e) {
+    System.out.println(e.toString());
+  }
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+
+  }
 }
