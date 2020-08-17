4261354d4c5178de55aa87c2f97a69af5da66b06
andrey_kazakov <aakazakovv@yandex.ru>
2020-08-17 11:03:22 +0300

    add private constructor

---
+++ filestorage-common/src/main/java/org/filestorage/common/Constants.java
@@ -0,0 +1,12 @@
+package org.filestorage.common;
+
+public class Constants {
+  
+  public static final byte PUT = -127;
+  public static final byte GET = -126;
+  public static final byte GET_LIST = -125;
+  public static final byte START = -124;
+  public static final byte END = -123;
+  
+  private Constants() { }
+}
